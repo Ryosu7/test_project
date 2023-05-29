2dc3919f1a06780f5858e037b8a74b4faa13442f
Ryosu <ryosu706@gmail.com>
2023-05-29 23:08:12 +0900

    Databinding commit

--- app/src/main/java/com/example/test_project/MainActivity.kt
+++ app/src/main/java/com/example/test_project/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.test_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.test_project.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        
+        binding.dataBindingEx.text = "바뀐 텍스트"
     }
 }
